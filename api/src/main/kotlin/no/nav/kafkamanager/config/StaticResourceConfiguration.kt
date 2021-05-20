5e7566bae0c7add76e5fa1002e4647c71ffd122b
Alexander Gård <alex.gaard@hotmail.com>
2021-05-20 13:23:03 +0200

    Redirect / to index.html

--- api/src/main/kotlin/no/nav/kafkamanager/config/StaticResourceConfiguration.kt
+++ api/src/main/kotlin/no/nav/kafkamanager/config/StaticResourceConfiguration.kt
@@ -2,11 +2,16 @@ package no.nav.kafkamanager.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
 class StaticResourceConfiguration : WebMvcConfigurer {
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addRedirectViewController("/", "/index.html")
+    }
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/**")
