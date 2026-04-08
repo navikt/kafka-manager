c49491ddf61e586193c00f3f2352e47e29f46a8b
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-04-08 11:49:23 +0200

    Endringer for å ta i bruk Spring Boot 4.0.5

--- api/src/main/kotlin/no/nav/kafkamanager/config/FilterConfig.kt
+++ api/src/main/kotlin/no/nav/kafkamanager/config/FilterConfig.kt
@@ -14,10 +14,10 @@ class FilterConfig {
     @Bean
     fun logFilterRegistrationBean(): FilterRegistrationBean<LogRequestFilter> {
         val registration = FilterRegistrationBean<LogRequestFilter>()
-        registration.filter = LogRequestFilter(
+        registration.setFilter(LogRequestFilter(
             EnvironmentUtils.getApplicationName().orElse(DEFAULT_APPLICATION_NAME),
             EnvironmentUtils.isDevelopment().orElse(false)
-        )
+        ))
         registration.order = 1
         registration.addUrlPatterns("/api/*")
         return registration
@@ -26,7 +26,7 @@ class FilterConfig {
     @Bean
     fun setStandardHeadersFilterRegistrationBean(): FilterRegistrationBean<SetStandardHttpHeadersFilter> {
         val registration = FilterRegistrationBean<SetStandardHttpHeadersFilter>()
-        registration.filter = SetStandardHttpHeadersFilter()
+        registration.setFilter(SetStandardHttpHeadersFilter())
         registration.order = 2
         registration.addUrlPatterns("/api/*")
         return registration
